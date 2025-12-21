4cab7a53cdc7111d63ebcac4e1d9cce6539655dd
DEVLE <n563212895131017@outlook.com>
2025-12-21 03:35:36 +0100

    Apply black formatting

--- test/test_belief_update.py
+++ test/test_belief_update.py
@@ -2,9 +2,7 @@ import os
 import sys
 import numpy as np
 
-sys.path.append(
-    os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src"))
-)
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src")))
 from Enviroment.belief_module import ParticleBeliefTracker
 
 
